eebae071e4d67470225966475a2221a636b13b12
Jacob Gonyer <jgonyer@darlingconsulting.com>
2020-05-14 09:35:06 -0400

    fixed travis file, modified server.js to use new env var

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
+const food = process.env.FOOD;
 async function main() {
   while(true) {
     console.log("Microservices rock!");
+    console.log(`I want to eat some ${food}`);
     await sleep(5000);
   }
 }
